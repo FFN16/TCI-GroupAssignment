32477a54dd9023201ec7b0e12418441f2b3a2239
Floris <nagsterza@gmail.com>
2021-10-31 16:44:28 +0100

    crt: ExamExecution

---
+++ src/main/java/exceptions/DataCleanedException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class DataCleanedException extends Exception{
+}
---
+++ src/test/java/execution/ExamExecutionTest.java
@@ -0,0 +1,78 @@
+package execution;
+
+import exceptions.*;
+import model.ExamID;
+import model.ExamSetup;
+import model.Student;
+import model.StudentExam;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class ExamExecutionTest {
+
+    /**
+     * @verifies create ExamExecution with valid parameters
+     * @see ExamExecution#ExamExecution(model.ExamSetup, java.util.List)
+     */
+    @Test
+    public void ExamExecution_shouldCreateExamExecutionWithValidParameters() throws Exception {
+        var examSetup = mock(ExamSetup.class);
+        var studentExams = Arrays.asList(mock(StudentExam.class), mock(StudentExam.class));
+
+        ExamExecution examExecution = new ExamExecution(examSetup, studentExams);
+
+        assertThat(examExecution.getExamSetup()).isEqualTo(examSetup);
+        assertThat(examExecution.getStudentExams()).isEqualTo(studentExams);
+    }
+
+    /**
+     * @verifies that ExamExecution cannot be cleared till finalized
+     * @see ExamExecution#clean()
+     */
+    @Test
+    public void ExamExecution_shouldNotClearTillFinalized() throws Exception {
+        var examSetup = mock(ExamSetup.class);
+        var studentExams = Arrays.asList(mock(StudentExam.class), mock(StudentExam.class));
+
+        ExamExecution examExecution = new ExamExecution(examSetup, studentExams);
+
+        Assertions.assertThrows(DataNotFinalizedException.class, examExecution::clean);
+    }
+
+    /**
+     * @verifies that when ExamExecution is cleaned info fields return DataCleanedException
+     * @see ExamExecution#clean()
+     */
+    @Test
+    public void ExamExecution_shouldClearOnceFinalized() throws Exception {
+        var examSetup = mock(ExamSetup.class);
+        var studentExams = Arrays.asList(mock(StudentExam.class), mock(StudentExam.class));
+
+        ExamExecution examExecution = new ExamExecution(examSetup, studentExams);
+
+        examExecution.setState(ExamExecutionState.FINALIZED);
+        examExecution.clean();
+
+        Assertions.assertThrows(DataCleanedException.class, examExecution::getExamSetup);
+        Assertions.assertThrows(DataCleanedException.class, examExecution::getStudentExams);
+    }
+
+    /**
+     * @verifies that getCopyStudentExams returns a copy of StudentExams
+     * @see ExamExecution#getCopyStudentExams()
+     */
+    @Test
+    public void ExamExecution_shouldReturnMatchingCopy() throws Exception {
+        var examSetup = mock(ExamSetup.class);
+        var studentExams = Arrays.asList(mock(StudentExam.class), mock(StudentExam.class));
+
+        ExamExecution examExecution = new ExamExecution(examSetup, studentExams);
+
+        assertThat(examExecution.getCopyStudentExams()).isEqualTo(studentExams);
+    }
+}
