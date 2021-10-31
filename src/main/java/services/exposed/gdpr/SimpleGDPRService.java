8a4821a369c6a2afe48895eaa1ca4b0699564716
Filippo <filnardocci1@gmail.com>
2021-11-01 00:13:14 +0100

    crt SimpleGDPRService

--- src/main/java/services/exposed/gdpr/SimpleGDPRService.java
+++ src/main/java/services/exposed/gdpr/SimpleGDPRService.java
@@ -3,11 +3,17 @@ package services.exposed.gdpr;
 import model.ExamID;
 import server.SimpleEFITserver;
 import services.exposed.ExamNotFoundException;
-import services.exposed.client.SimpleEFITClientService;
-import services.exposed.gdpr.GDPRInterface;
 
 public class SimpleGDPRService implements GDPRInterface {
 
+    /**
+     *@should throw ExamNotFoundException
+     *@should throw examNotFinalizedException
+     */
+    //Leaving it here so that TestCherry recognizes @should
+    public SimpleGDPRService(){
+
+    }
     private SimpleEFITserver server;
 
     public SimpleEFITserver getServer() {
@@ -22,5 +28,6 @@ public class SimpleGDPRService implements GDPRInterface {
     public void removeStudentExamData(ExamID examID) throws ExamNotFoundException, ExamNotFinalizedException {
 
     }
-    // TODO
+
+
 }
